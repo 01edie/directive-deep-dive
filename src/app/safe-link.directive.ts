4e93036087c90de965ec1711ccd7afb3980f21ac
BISWAJIT BOSE <97077410+01edie@users.noreply.github.com>
2024-11-13 17:48:20 +0530

    hostDirectives config

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, inject, input } from '@angular/core';
+import { LogDirective } from './log.directive';
 
 @Directive({
   selector: 'a[safeLinkDirective]',
@@ -6,6 +7,7 @@ import { Directive, ElementRef, inject, input } from '@angular/core';
   host: {
     '(click)': 'onLeaveSite($event)',
   },
+  hostDirectives: [LogDirective],
 })
 export class SafeLinkDirective {
   queryParam = input('myValue', { alias: 'safeLinkDirective' }); // {alias:'safeLinkDirective'}
